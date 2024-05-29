89a5cd83b8aeb083888c49a6021eabc897c7cd9b
sandeep <sandeepk6858@gmail.com>
2024-05-29 16:07:36 +0530

    support page done with popup on report site button

--- src/app/support/page.js
+++ src/app/support/page.js
@@ -1,9 +1,106 @@
-import React from 'react'
+'use client'
+import { useState } from 'react';
+import Modal from '@/components/popup/popup';
 
 const support = () => {
-  return (
-    <div>support</div>
-  )
-}
+    const [activeTab, setActiveTab] = useState('step1');
 
-export default support
+    const renderContent = () => {
+        switch (activeTab) {
+            case 'step1':
+                return <div>
+
+                <div class="text-gray-600 body-font">
+                  <div class="container px-[10px]  py-[60px] md:py-[80px] mx-auto">
+                    <div class="flex flex-wrap -m-4">
+                      <div class="p-2 mb-[10px] md:p-4  lg:w-1/3">
+                        <div class="h-full bg-gray-100 shadow-lg bg-opacity-75 px-8 pt-16 py-16rounded-lg overflow-hidden relative">
+                          <h2 className="text-[16px] leading-[19px] text-blueprimary font-[600] mb-[10px] mt-[10px]">What is Lorem Ipsum?</h2>
+                          <p className="leading-relaxed text-base text-grey mb-[40px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        </div>
+                      </div>
+                      <div class="p-2 mb-[10px] md:p-4 lg:w-1/3">
+                        <div class="h-full bg-gray-100 shadow-lg bg-opacity-75 px-8 pt-16 py-16rounded-lg overflow-hidden relative">
+                          <h2 className="text-[16px] leading-[19px] text-blueprimary font-[600] mb-[10px] mt-[10px]">Where does it come from?</h2>
+                          <p className="leading-relaxed text-base text-grey mb-[40px]">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetu</p>
+                        </div>
+                      </div>
+                      <div class="p-2 mb-[10px] md:p-4 lg:w-1/3">
+                        <div class="h-full bg-gray-100 shadow-lg bg-opacity-75 px-8 pt-16 py-16rounded-lg overflow-hidden relative">
+                          <h2 className="text-[16px] leading-[19px] text-blueprimary font-[600] mb-[10px] mt-[10px]">Why do we use it?</h2>
+                          <p className="leading-relaxed text-base text-grey mb-[40px]">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+            </div>;
+            case 'step2':
+                return <div>
+
+<div class="text-gray-600 body-font">
+                  <div class="container px-[10px] py-[60px] md:py-[80px] mx-auto">
+                    <div class="flex flex-wrap -m-4">
+                      <div class="p-2 mb-[10px] md:p-4 lg:w-1/3">
+                        <div class="h-full bg-gray-100 shadow-lg bg-opacity-75 px-8 pt-16 py-16 rounded-lg overflow-hidden relative w-[310px]">
+                          <h2 className="text-[16px] leading-[19px] text-blueprimary font-[600] mb-[10px] mt-[10px]">Question2</h2>
+                          <h2 className="text-[16px] leading-[19px] text-grey font-[600] mb-[10px] mt-[30px]">Answer2</h2>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            
+            </div>;
+            case 'step3':
+                return <div>
+
+              <div class="text-gray-600 body-font">
+                  <div class="container px-[10px] py-[60px] md:py-[80px] mx-auto">
+                    <div class="flex flex-wrap -m-4">
+                      <div class="p-2 mb-[10px] md:p-4 lg:w-1/3">
+                        <div class="h-full bg-gray-100 shadow-lg bg-opacity-75 px-8 pt-16 py-16rounded-lg overflow-hidden relative">
+                          <h2 className="text-[16px] leading-[19px] text-blueprimary font-[600] mb-[10px] mt-[10px]">What is Lorem Ipsum?</h2>
+                          <p className="leading-relaxed text-base text-grey mb-[40px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                </div>;
+            default:
+                return null;
+        }
+    };
+
+    return (
+      <section className="text-gray-600 body-font">
+      <div className="container px-[10px] py-[60px] md:py-[80px] mx-auto flex flex-wrap flex-col">
+        <div className='flex flex-wrap justify-center gap-[0px] sm:gap-[0px] md:gap-[0px] lg:gap-[40px] w-[100%]'>
+          <div className=' flex overflow-auto tabs_div'>
+          <button className={`px-6 py-3 w-1/2 sm:w-auto justify-center whitespace-nowrap sm:justify-start text-[16px] font-medium inline-flex items-center leading-none text-bluesecondary`}>
+            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5 mr-3 text-orangeprimary" viewBox="0 0 24 24">
+              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg> Help Center </button>
+          <div className="flex mx-auto">
+            <button onClick={()=> setActiveTab('step1')} className={`px-6 py-3 w-1/2 sm:w-auto justify-center whitespace-nowrap sm:justify-start border-b-2 text-[14px]font-medium inline-flex items-center leading-none ${ activeTab === 'step1' ? 'bg-gray-100 border-bluesecondary text-bluesecondary' : 'border-gray-200 hover:text-gray-900' } tracking-wider`} > Do you need help? </button>
+            <button onClick={()=> setActiveTab('step2')} className={`px-6 py-3 w-1/2 sm:w-auto justify-center whitespace-nowrap sm:justify-start border-b-2 text-[14px]font-medium inline-flex items-center leading-none ${ activeTab === 'step2' ? 'bg-gray-100 border-bluesecondary text-bluesecondary' : 'border-gray-200 hover:text-gray-900' } tracking-wider`} > Contact </button>
+            <button onClick={()=> setActiveTab('step3')} className={`px-6 py-3 w-1/2 sm:w-auto justify-center whitespace-nowrap sm:justify-start border-b-2 text-[14px]font-medium inline-flex items-center leading-none ${ activeTab === 'step3' ? 'bg-gray-100 border-bluesecondary text-bluesecondary' : 'border-gray-200 hover:text-gray-900' } tracking-wider`} > How can we help you today </button> 
+          </div>
+          </div>
+         <div className='flex gap-[30px] button_tabs_right'>
+          {/* <button className={`bg-orangeprimary rounded-[30px] text-[#fff] px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start text-[14px]font-medium inline-flex items-center leading-none `}> Report Site </button> */}
+          <Modal />
+            <button className={`bg-orangeprimary rounded-[30px] text-[#fff] px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start text-[14px]font-medium inline-flex items-center leading-none `}> Contact Support </button>
+         </div>
+        </div>
+        <div className="p-2 mb-[10px] md:p-4">{renderContent()}</div>
+      </div>
+    </section>
+    );
+};
+
+export default support;
---
+++ src/components/popup/popup.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+
+const Modal = () => {
+  const [openModal, setModal] = useState(false);
+
+  const handleModal = () => {
+    setModal(!openModal);
+  };
+
+  return (
+    <div>
+      <button
+        type="button"
+        className="bg-orangeprimary rounded-[30px] text-[#fff] px-6 py-3 sm:w-auto justify-center sm:justify-start text-[14px] font-medium inline-flex items-center leading-none w-[160px] h-[44px]"
+        onClick={handleModal}
+      >
+        Report Site
+      </button>
+      {openModal && (
+        <div className="fixed z-[1] top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex justify-center items-center">
+          <div className="w-[460px] bg-white shadow-lg py-[20px] px-[20px] rounded-md relative">
+            <button
+              className="absolute top-2 right-2 text-gray-600 hover:text-gray-900 pr-[10px] pt-[10px] text-[30px]"
+              onClick={handleModal}
+            >
+              &times;
+            </button>
+            <form className='p-[20px]'>
+            <h2 className='text-[20px] font-semibold mb-[30px] border-b-2 pb-[20px]'>Report Site</h2>
+              <div className="mb-5">
+                <label
+                  htmlFor="name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              <div className="mb-5">
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              <div className="mb-5">
+                <label
+                  htmlFor="issue"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Describe the Issue
+                </label>
+                <textarea
+                  id="issue"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="bg-orangeprimary rounded-[30px] text-[#fff] px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start text-[14px]font-medium inline-flex items-center leading-none "
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Modal;
