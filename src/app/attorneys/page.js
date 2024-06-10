86f678b445b47d32bbaeca8793ccedeb8d724f3f
sandeepk6858 <sandeepk6858@gmail.com>
2024-06-10 17:27:48 +0530

    attorney page design

---
+++ src/app/attorneys/page.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Filter from "@/components/Filter/Filter";
+import SupportCard from "@/components/card/support";
+import JobSlider from "@/components/JobSlider/JobSlider";
+
+const Attorneys = () => {
+  return (
+    <>
+    <JobSlider />
+    <Filter />
+    <section className="text-blackcolor body-font overflow-hidden">
+        <div className="px-[15px] lg:px-[40px] py-[60px] md:py-[80px] mx-auto ">
+          <div className="flex flex-wrap lg:flex-nowrap gap-[20px] xl:gap-[30px]">
+          <SupportCard />
+            <SupportCard />
+            <SupportCard />
+            <SupportCard />
+          </div>
+        </div>
+      </section>
+    </>
+);
+};
+
+export default Attorneys;
