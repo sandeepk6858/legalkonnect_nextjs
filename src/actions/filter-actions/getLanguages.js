4671134b4f27fa5f98354487836a4b56d64e0e56
Sandeep Kumar <34616550+sandeepk6858@users.noreply.github.com>
2024-08-09 10:32:54 +0530

    get languages function

---
+++ src/actions/filter-actions/getLanguages.js
@@ -0,0 +1,29 @@
+"use server"
+import { user_token } from '@/components/utils/helper/helper';
+import { revalidatePath } from 'next/cache'
+import axios from 'axios';
+
+export async function fetchLanguagesData() {
+    try {
+        const headers = {
+            'Authorization': `Bearer ${user_token()}`,
+            'Content-Type': 'application/json'
+        };
+        // const body = {
+        //     filter,
+        //     sort
+        // };
+        const response = await axios.get(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/locations`,
+            {
+                headers: {
+                    ...headers
+                }
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching jobs:", error.message);
+        return null;
+    }
+}
